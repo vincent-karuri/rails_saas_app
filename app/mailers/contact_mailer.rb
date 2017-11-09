d634117181e47a971de96f154a21a6eebcf3df02
Vincent Karuri <vincentmkaruri@gmail.com>
2017-11-09 21:35:36 +0000

    removed bugs in ContactMailer class

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
-class ContactMailer < ActionMailer :: Base
-    default to 'vincentmkaruri@gmail.com'
+class ContactMailer < ActionMailer::Base
+    default to: 'vincentmkaruri@gmail.com'
     
     def contact_email(name, email, body) 
         @name = name
