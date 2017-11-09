0b77fe19f69bc3a05556f88104ddd1ae52c80850
Vincent Karuri <vincentmkaruri@gmail.com>
2017-11-09 22:32:11 +0000

    Added comments

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,25 +1,36 @@
 class ContactsController < ApplicationController
+    # GET request to /contact-us
     def new
         @contact = Contact.new
     end
     
+    # POST request /contacts
     def create
+        # mass assignment of form fields
         @contact = Contact.new(contact_params)
+        
+        # save Contact obj to db
         if @contact.save
+            # field params
             name = params[:contact][:name]
             email = params[:contact][:email]
             body = params[:contact][:comments]
             
+            # plug variables into contact mailer
             ContactMailer.contact_email(name, email, body).deliver
+            
+            # flash success message
             flash[:success] = "Message sent."
             redirect_to new_contact_path
         else
+            # flash fail message with errors
             flash[:danger] = @contact.errors.full_messages.join(',')
             redirect_to new_contact_path
         end
     end
     
     private
+        # collect data from form
         def contact_params
             params.require(:contact).permit(:name, :email, :comments)
         end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ActiveRecord::Base
+    # validate contact
     validates :name, presence: true
     validates :email, presence: true
     validates :comments, presence: true
