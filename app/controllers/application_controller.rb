84f7ae126ea6c0777b9ef9d2503cf361d88e75c5
Vincent Karuri <vincentmkaruri@gmail.com>
2017-11-24 20:18:18 +0000

    Added code comments for Stripe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
+  # white-list following form fields form devise
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
     def configure_permitted_parameters
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+    # Extend default devise gem to take care of pro user and free user
+    # subscription by subscribing pro users to Stripe
     def create
         super do |resource|
+            # check for plan type and if pro, save the token from Stripe
+            # as you save the new user. Else, just save the user in the 
+            # normal way without adding a user token.
             if params[:plan]
                resource.plan_id = params[:plan] 
                if resource.plan_id == 2
