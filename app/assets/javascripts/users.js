f72b6184d6450e832ae1219b038f1783d5224173
Vincent Karuri <vincentmkaruri@gmail.com>
2017-11-24 16:22:58 +0000

    Added stripe user subscription enrollment capability

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,25 +1,63 @@
 /* global $, Stripe */
 $(document).on('turbolinks:load', function(){
     var theForm = $("#pro-form");
-    var submitBtn = $("#form-submit-btn");
+    var submitBtn = $("#form-signup-btn");
     
     // set Stripe publice key
     Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
     
     submitBtn.click(function(event){
         event.preventDefault()
+        submitBtn.val("Processing").prop('disabled', true);
+        
         // collect credit card fields
         var ccNum = $('#card_number').val(),
             cvcNum = $('#card_code').val(),
             expMonth = $('#card_month').val(),
             expYear = $('#card_year').val();
             
-        Stripe.createToken({
-            number: ccNum,
-            cvc: cvcNum,
-            exp_month: expMonth,
-            exp_year: expYear
-        }, stripeResponseHandler);
+        // validate card fields
+        var error = false;
+        
+        if (!Stripe.card.validateCardNumber(ccNum)) {
+            error = true;
+            alert("The credit card number appears to be invalid");
+        }
+        
+        // validate cvc number
+        if (!Stripe.card.validateExpiry(ccNum)) {
+            error = true;
+            alert("The CVC number appears to be invalid");
+        }
+        
+        // validate expiration date
+        if (!Stripe.card.validateCardNumber(ccNum)) {
+            error = true;
+            alert("The credit card expiration date appears to be invalid");
+        }
+        
+        if (error) {
+            submitBtn.prop('disabled', false).val("Sign Up");
+        } else {
+            // send card info to Stripe
+            Stripe.createToken({
+                number: ccNum,
+                cvc: cvcNum,
+                exp_month: expMonth,
+                exp_year: expYear
+            }, stripeResponseHandler);
+        }
+        return false;
     });
     
+    // Stripe returns token
+    function stripeResponseHandler(status, response) {
+        var token = response.id;
+        
+        // inject card token in hidden field
+        theForm.append($('<input type="hidden" name="user[stripe_card_token]">').val(token));
+        
+        // submit form
+        theForm.get(0).submit();
+    }
 });
--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20171123200210) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.integer  "planid"
+    t.integer  "plan_id"
     t.string   "stripe_customer_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
