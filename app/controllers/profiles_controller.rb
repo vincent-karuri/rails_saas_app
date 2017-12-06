97d4b730b01819895a1d24c3db9eb558bdd9f7cd
Vincent Karuri <vincentmkaruri@gmail.com>
2017-12-06 08:40:15 +0000

    Added user profile creation form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,5 +3,6 @@ class ProfilesController < ApplicationController
     # GET to /users/:user_id/profile/new
     def new
         # render blank profile details form
+        @profile = Profile.new
     end
 end
